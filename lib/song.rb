c356881c471afd86e227ef0bf3ffa32560ab007e
Allan Farca <allan.farca@gmail.com>
2020-04-14 07:18:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,9 +51,8 @@ class Song
     song_array = []
     song_array = string.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
-    song = self.new
-    song.name = song_array[0]
-    song.artist_name = song_array[1]
+    self.new.name = song_array[0]
+    self.new.artist_name = song_array[1]
   end 
 
 end
